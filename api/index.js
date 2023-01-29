b3cbafffcdc8cad6b3eeb9c44749bfb35938d424
navivix <gkaczmarek2304@gmail.com>
2023-01-29 21:47:55 +0100

    Create endpoints

--- api/index.js
+++ api/index.js
@@ -1,12 +1,45 @@
 import * as dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+import ShortUrl from "./models/shortUrl.js";
 
 dotenv.config();
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+
+  res.send(shortUrls);
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
+});
+
+app.post("/", async (req, res) => {
+  const shortUrl = await ShortUrl.create({ full: req.body.url });
+
+  res.send(shortUrl);
+});
+
+app.delete("/:shortUrl", async (req, res) => {
+  await ShortUrl.deleteOne({ short: req.params.shortUrl });
+
+  res.send();
 });
 
 app.listen(process.env.PORT);
